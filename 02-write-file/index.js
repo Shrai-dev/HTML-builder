bf793d666af2650258c329fc3c9ade2f276dd40f
Volodymyr <sydorenko.volodymyr1995@gmail.com>
2022-05-22 15:09:46 +0300

    feat: add solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
+const { stdin, stdout } = require('process');
+
+const absolutePath = path.join('02-write-file', 'text.txt');
+const result = fs.createWriteStream(absolutePath);
+const rl = readline.createInterface({ input: stdin, output: stdout });
+
+stdout.write('Enter your question about Node.js\n');
+result.write('Enter your question about Node.js\n');
+
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    process.exit();
+  } else {
+    result.write(`Your question: ${input}\n`)
+    stdout.write(`Your question: ${input}\n`)
+    stdout.write('Next question\n')
+  }
+});
+
+rl.on('SIGINT', () => {
+  process.exit();
+});
+
+process.on('exit', () => stdout.write('Have a nice day!'));
